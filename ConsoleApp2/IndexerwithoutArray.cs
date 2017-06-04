28036d93a8d884ae6068e81cf1eefe98abe5a41b
Administrator <Administrator@ADC-1>
2017-06-04 12:21:39 +0530

    Completed

--- ConsoleApp2/IndexerwithoutArray.cs
+++ ConsoleApp2/IndexerwithoutArray.cs
@@ -8,12 +8,65 @@ namespace IndexerwithoutArrayNS
 {
     class IndexerwithoutArray
     {
+        int maxpower;
+        
+        public IndexerwithoutArray(int power)
+        {
+            maxpower = power;
+        }
+        public bool IsVaild(int size)
+        {
+            if(size >= 0 & size < maxpower)
+            {
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public int this[int power]
+        {
+            get
+            {
+                int result = 1;
+                for(int i =0; i<power;i++)
+                {
+                    result = result * 2;
+                }
+                return result;
+            }
+        }
 
 
+
+
+    }
+
+    class UseIndexerwithoutArray
+    {
 
 
         static void Main(string[] args)
         {
+            IndexerwithoutArray indexerobj = new IndexerwithoutArray(9);
+            int PowerOf2;
+            for (int i = 0; i < 50; i++)
+            {
+                if(indexerobj.IsVaild(i))
+                {
+                    PowerOf2 = indexerobj[i];
+                    Console.WriteLine("2 to the Power of {0} = {1} ",i,PowerOf2);
+                }
+
+            }
+
+            Console.Read();
+
+
         }
     }
 }
